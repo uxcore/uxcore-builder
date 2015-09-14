daf9812ab549f2f8ca1f7707fb1437d27f64af79
光弘 <guanghong.wsj@alibaba-inc.com>
2015-09-14 11:57:05 +0800

    remove dependencies

--- src/Builder.js
+++ src/Builder.js
@@ -7,21 +7,4 @@
  */
 
 
-module.exports = {
-    Button: require("uxcore-button"),
-    Calendar: require("uxcore-calendar"),
-    CheckboxGroup: require("uxcore-checkbox-group"),
-    Dialog: require("uxcore-dialog"),
-    Dropdown: require("uxcore-dropdown"),
-    Form: require("uxcore-form"),
-    Grid: require("uxcore-grid"),
-    Pagination: require("uxcore-pagination"),
-    RcUpload: require("uxcore-rc-upload"),
-    Select2: require("uxcore-select2"),
-    Tabs: require("uxcore-tabs"),
-    Tooltip: require("uxcore-tooltip"),
-    Tree: require("uxcore-tree"),
-    Transfer: require("uxcore-transfer"),
-    CheckboxGroup: require("uxcore-checkbox-group"),
-    RadioGroup: require("uxcore-radiogroup")
-}
+module.exports = {}
